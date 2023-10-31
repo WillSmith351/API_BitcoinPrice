c5ff45eb0dad2af5bdbeea150828929289b216a3
WillSmith351 <williamcalvertsmith@gmail.com>
2023-10-31 23:46:41 +0100

    Adding errors and managing errors

--- btc.js
+++ btc.js
@@ -2,14 +2,32 @@
 
 const axios = require('axios');
 
-async function main(){
+async function main() {
+    try {
+        const response = await axios.get('https://api.coindesk.com/v1/bpi/currentprice.json');
 
-    const response = await axios.get('https://api.coindesk.com/v1/bpi/currentprice.json');
+        let currency;
 
-    const updatedAt = response.data.time.updated
-    const rate = response.data.bpi['EUR'].rate
+        if (process.argv[2]) {
+            currency = process.argv[2].toUpperCase();
+        }
+        else {
+            currency = 'USD'
+        }
 
-    console.log(`1 BTC = ${rate} EUR (${updatedAt})`)
+
+        if (!response.data.bpi[currency]) {
+            throw new Error('Devise inconnue')
+        }
+
+        const updatedAt = response.data.time.updated;
+        const rate = response.data.bpi[currency].rate;
+
+        console.log(`1 BTC = ${rate} ${currency} (${updatedAt})`)
+    }
+    catch (err) {
+        console.error(err.toString());
+    }
 }
 
 main()
