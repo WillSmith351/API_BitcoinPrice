a950e544f060a019880ddcf2c5169e51400fd0e2
WillSmith351 <williamcalvertsmith@gmail.com>
2023-11-01 19:20:33 +0100

    Adding code documentation

--- btc.js
+++ btc.js
@@ -1,27 +1,30 @@
-#!/usr/bin/env node
+// Fetches the current Bitcoin price in a specified currency using the Coindesk API.
 
 const axios = require('axios');
 
 async function main() {
-    const currency = process.argv[2] 
-    ? process.argv[2].tuUpperCase()
-    () : 'USD'
+    // Set the currency based on the user input or default to USD.
+    const currency = process.argv[2] ? process.argv[2].toUpperCase() : 'USD';
 
     try {
+        // API endpoint for fetching Bitcoin price data.
         const endpoint = 'https://api.coindesk.com/v1/bpi/currentprice.json';
         const { data } = await axios.get(endpoint);
         const updatedAt = data.time.updated;
-        const rate = data.bpi[currency].rate;
 
+        // Check if the requested currency is available.
         if (!data.bpi[currency]) {
-            throw new Error('Devise inconnue')
+            throw new Error('Unknown currency');
         }
 
-        console.log(`1 BTC = ${rate} ${currency} (${updatedAt})`)
-    }
-    catch (err) {
+        // Retrieve the current Bitcoin rate in the specified currency and log it with the timestamp.
+        const rate = data.bpi[currency].rate;
+        console.log(`1 BTC = ${rate} ${currency} (${updatedAt})`);
+    } catch (err) {
+        // Handle any errors that occur during the process and log them to the console.
         console.error(err.toString());
     }
 }
 
-main()
+// Execute the main function to fetch and display the Bitcoin price.
+main();
